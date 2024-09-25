8e98399dbd84c4bdef0eefe0b68d4b2c14234ab1
tesfahiwet trunh <eminemmerne@gmail.com>
2024-09-25 20:05:31 +0300

    Auth_App views to CBVs

--- Auth_App/views.py
+++ Auth_App/views.py
@@ -1,6 +1,7 @@
 import random
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -9,14 +10,18 @@ from django.http import JsonResponse
 from captcha.helpers import captcha_image_url
 from captcha.models import CaptchaStore
 
+class LoginView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect("admin-dashboard" if request.user.is_superuser else "sponsor-home-page")
+        
+        # Generate CAPTCHA for GET requests
+        captcha_key = CaptchaStore.generate_key()
+        captcha_image = captcha_image_url(captcha_key)
 
-def render_login_page(request):
-    if request.user.is_authenticated:
-        return redirect(
-            "admin-dashboard" if request.user.is_superuser else "sponsor-home-page"
-        )
+        return render(request, "login.html", {"captcha_image": captcha_image, "captcha_key": captcha_key})
 
-    if request.method == "POST":
+    def post(self, request):
         username = request.POST.get("login_username")
         password = request.POST.get("login_password")
         captcha_key = request.POST.get("captcha_0")  # Captcha key
@@ -25,7 +30,6 @@ def render_login_page(request):
         # Validate CAPTCHA
         try:
             captcha_obj = CaptchaStore.objects.get(hashkey=captcha_key)
-            # Case-insensitive comparison
             if captcha_obj.response.lower() != captcha_value.lower():
                 messages.error(request, "Invalid CAPTCHA.")
                 return redirect("admin-login")
@@ -37,28 +41,23 @@ def render_login_page(request):
         user_auth = authenticate(request, username=username, password=password)
         if user_auth is not None:
             login(request, user_auth)
-            return redirect(
-                "admin-dashboard" if user_auth.is_superuser else "sponsor-home-page"
-            )
+            return redirect("admin-dashboard" if user_auth.is_superuser else "sponsor-home-page")
         else:
             messages.error(request, "Invalid username or password.")
             return redirect("admin-login")
 
-    # Generate CAPTCHA for GET requests
-    captcha_key = CaptchaStore.generate_key()
-    captcha_image = captcha_image_url(captcha_key)
 
-    return render(request, "login.html", {"captcha_image": captcha_image, "captcha_key": captcha_key})
-
-def render_logout(request):
-
-    logout(request)
-
-    return redirect("admin-login")
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect("admin-login")
 
 
-def render_create_account_page(request):
-    if request.method == "POST":
+class CreateAccountView(View):
+    def get(self, request):
+        return render(request, "create_account.html")
+
+    def post(self, request):
         # Get the form inputs
         username = request.POST.get("username")
         first_name = request.POST.get("first_name")
@@ -85,7 +84,7 @@ def render_create_account_page(request):
             password=make_password(password),  # Password is hashed for security
         )
 
-        # Create associated SponosrAccount
+        # Create associated SponsorAccount
         SponosrAccount.objects.create(user=user, phone_number=phone)
 
         # Return JSON response
@@ -93,9 +92,7 @@ def render_create_account_page(request):
             {"success": True, "message": "Account created successfully!"}
         )
 
-    return render(request, "create_account.html")
 
-
-def render_forgot_password_page(request):
-
-    return render(request, "forgot_password.html")
+class ForgotPasswordView(View):
+    def get(self, request):
+        return render(request, "forgot_password.html")
--- Hidrina_Website/urls.py
+++ Hidrina_Website/urls.py
@@ -7,10 +7,10 @@ from User_App.views import home_view
 from User_App.views import FamiliesListPage
 from User_App.views import AboutUsPage
 # import Auth_App urls
-from Auth_App.views import render_login_page
-from Auth_App.views import render_logout
-from Auth_App.views import render_forgot_password_page
-from Auth_App.views import render_create_account_page
+from Auth_App.views import (LoginView, 
+                            LogoutView, 
+                            ForgotPasswordView, 
+                            CreateAccountView)
 # import Sponsor_App urls
 from Sponsor_App.views import home_page
 from Sponsor_App.views import my_sponsorship
@@ -32,22 +32,20 @@ urlpatterns = [
     path('families-list/', FamiliesListPage, name='family-list-page'),
     path('about-us/', AboutUsPage, name='about-us-page'),
     # url pattern for Auth_App
-    path('login-page/', render_login_page, name = 'admin-login'),
-    path('logout-page/', render_logout, name = 'admin-logout'),
-    path('forgot-password/', render_forgot_password_page, name = 'forgot-password'),
-    path('create-account/', render_create_account_page, name = 'create-account'),
+    path('login-page/', LoginView.as_view(), name='admin-login'),
+    path('logout-page/', LogoutView.as_view(), name='admin-logout'),
+    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
+    path('create-account/', CreateAccountView.as_view(), name='create-account'),
     # url pattern for Sponsor_App
-    path('home/', home_page, name = 'sponsor-home-page'),
-    path('my-sponsorship/', my_sponsorship, name = 'my-sponsorship'),
+    path('home/', home_page, name='sponsor-home-page'),
+    path('my-sponsorship/', my_sponsorship, name='my-sponsorship'),
     # url pattern for Super_Admin_Base
-    path('dashboard/', dashboard, name = 'admin-dashboard'),
-    path('add-family/', AddFamily, name = 'add-family'),
-    path('family-management/', FamilyManagement, name = 'family-management'),
+    path('dashboard/', dashboard, name='admin-dashboard'),
+    path('add-family/', AddFamily, name='add-family'),
+    path('family-management/', FamilyManagement, name='family-management'),
     path('family/<int:pk>/update/', FamilyListUpdateView.as_view(), name='family-update'),
     path('family/<int:pk>/delete/', FamilyDeleteView.as_view(), name='family-delete'),
     path('family/image/<int:image_id>/update/', UpdateFamilyImage, name='update_family_image'),
     path('family/image/<int:image_id>/delete/', DeleteFamilyImage, name='delete_family_image'),
     path('export-data', ExportFamilyData, name='export-family-data')
-
-
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
